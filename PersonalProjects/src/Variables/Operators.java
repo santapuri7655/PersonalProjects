585430219151602f1061580ceede3092a198be5c
Radhika Santapuri <radhika.santapuri@gmail.com>
2019-04-09 21:56:33 -0500

    operators

---
+++ PersonalProjects/src/Variables/Operators.java
@@ -0,0 +1,29 @@
+package Variables;
+
+// 1. Create a double variable with the value 20
+// 2. Create a second variable of type double with the value 80
+// 3. Add both numbers up and multiply by 25
+// 4. Use the remainder operator to figure out the remainder from the sum of #3 divided by 40
+// 5. Write an "if" statement that displays a message "Total was over the limit"
+//    if the remaining total (#4) is equal to 20 or less.
+
+public class Operators {
+
+    public static void main(String[] args)
+    {
+        double firstVariable = 20;
+        double secondVariable = 80;
+
+        double sum = firstVariable + secondVariable;
+        System.out.println("sum of double variables is " + sum);
+
+        double multiply = sum * 25;
+        System.out.println("multiply sum by 25 " + multiply);
+
+        double remainder = multiply % 40;
+        System.out.println("remainder from sum divided by 40 is " + remainder);
+
+        if(remainder <= 20)
+            System.out.println("Total was over the limit");
+    }
+}
