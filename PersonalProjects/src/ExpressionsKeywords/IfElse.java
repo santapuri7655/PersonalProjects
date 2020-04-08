cac494b58721c1e73c729895b16b1f83b7b1cd1a
Radhika Santapuri <radhika.santapuri@gmail.com>
2020-04-08 15:59:30 -0700

    more lecture exercises

--- PersonalProjects/src/ExpressionsKeywords/IfElse.java
+++ PersonalProjects/src/ExpressionsKeywords/IfElse.java
@@ -3,30 +3,32 @@ package ExpressionsKeywords;
 public class IfElse {
     public static void main(String[] args) {
 
-        // Print out a second score on the screen with the following
-        // score set to 10000
+        // Print out a second score1 on the screen with the following
+        // score1 set to 10000
         // levelCompleted set to 8
         // bonus set to 200
         // But make sure the first printout above still displays as well
 
         boolean gameOver = true;
         int score = 10000;
-        int levelComplete = 8;
+        int levelCompleted = 8;
         int bonus = 200;
 
-        if(score < 10000 && score > 1000) {
-            System.out.println("your score was less than 5000 and greater than 1000");
-        }
-        else if(score < 1000) {
-            System.out.println("your score is less than 1000");
-        }
-        else {
-            System.out.println("got here");
-        }
+        calculateScore(true, 800, 5, 100);
 
         if(gameOver) {
-            int finalScore = score + (levelComplete * 5);
+            int finalScore = score + (levelCompleted * bonus);
             System.out.println("your final score was " + finalScore);
         }
+
+    }
+
+    public  static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
     }
 }
