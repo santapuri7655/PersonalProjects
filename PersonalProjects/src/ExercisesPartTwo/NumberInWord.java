00ba9df7d52a74d7ab00c95b82358ba33c759c36
Radhika Santapuri <radhika.santapuri@gmail.com>
2020-04-09 17:18:16 -0700

    control flow statements switch exercises on switch statements

--- PersonalProjects/src/ExercisesPartTwo/NumberInWord.java
+++ PersonalProjects/src/ExercisesPartTwo/NumberInWord.java
@@ -1,26 +1,21 @@
 package ExercisesPartTwo;
 
-public class NumberInWord
-{
-    /**
-     * Write a method called printNumberInWord.
-     * The method has one parameter number which is the whole number.
-     * The method needs to print "ZERO", "ONE", "TWO", ... "NINE", "OTHER" if the int parameter number is 0, 1, 2, .... 9
-     * or other for any other number including negative numbers.
-     * You can use if-else statement or switch statement whatever is easier for you.
+public class NumberInWord {
+
+    /*
+    Write a method called printNumberInWord. The method has one parameter number which is the whole number.
+    The method needs to print "ZERO", "ONE", "TWO", ... "NINE", "OTHER" if the int parameter number is 0, 1, 2, .... 9
+    or other for any other number including negative numbers.
+    You can use if-else statement or switch statement whatever is easier for you.
+
+NOTE: Method printNumberInWord needs to be public static for now, we are only using static methods.
+
+NOTE: Do not add main method to solution code.
      */
 
-    public static void main(String[] args)
-    {
-        printNumberInWord(9);
-        printNumberInWord(-1);
-        printNumberInWord(20);
-        printNumberInWord(6);
-    }
-    public static void printNumberInWord(int wholeNumber)
-    {
-        switch (wholeNumber)
-        {
+    public static void printNumberInWord(int number) {
+
+        switch (number) {
             case 0:
                 System.out.println("ZERO");
                 break;
@@ -51,9 +46,11 @@ public class NumberInWord
             case 9:
                 System.out.println("NINE");
                 break;
+
             default:
                 System.out.println("OTHER");
                 break;
+
         }
     }
 }
