5d1d8ab91dbee670c9a575663b86f93747076d5f
Radhika Santapuri <radhika.santapuri@gmail.com>
2020-04-10 21:43:12 -0700

    coding exercises

--- PersonalProjects/src/ControlFlowStatements/WhileLoop.java
+++ PersonalProjects/src/ControlFlowStatements/WhileLoop.java
@@ -2,6 +2,66 @@ package ControlFlowStatements;
 
 public class WhileLoop {
 
+    public static void main(String[] args)
+    {
+        int count = 1;
+        while(count != 6) {
+            System.out.println("number is " + count);
+            count++;
+        }
+
+        count = 1;
+        while(true) {
+            if(count == 6) {
+                break;
+            }
+            System.out.println("number is " + count);
+            count++;
+        }
+
+        int number = 4;
+        int finishNumber = 20;
+        count = 0;
+        int sum = 0;
+// Sum of even numbers
+        while (number <= finishNumber) {
+            number++;
+            if(!isEvenNumber(number)){
+                continue;
+            }
+            sum = sum + number;
+            System.out.println("number is " + number);
+        }
+        System.out.println("Total sum = " + sum);
+
+// Total number of even numbers with a range
+        while (number <= finishNumber) {
+            if(isEvenNumber(number)){
+                System.out.println(number);
+                count++;
+            }
+            number++;
+        }
+        System.out.println("Number of even numbers found = " + count);
+
+// Break after 5 even numbers
+        while (true) {
+            if(isEvenNumber(number)) {
+                System.out.println(number);
+                count++;
+                if(count >= 5){
+                    break;
+                }
+            }
+            number++;
+            if(number > finishNumber) {
+                break;
+            }
+        }
+    }
+
+
+
     /**
      * Create a method called isEvenNumber that takes a parameter of type int
      * Its purpose is to determine if the argument passed to the method is
@@ -9,30 +69,11 @@ public class WhileLoop {
      * return true if an even number, otherwise return false;
      */
 
-    public static void main(String[] args)
-    {
-        int firstNumber = 56;
-        int finishNumber = 98;
-
-        while(firstNumber <= finishNumber)
-        {
-            firstNumber++;
-            if(isEvenNumber(firstNumber))
-            {
-                System.out.println(firstNumber);
-            }
-        }
-    }
-    public static boolean isEvenNumber(int number)
-    {
-        if(number % 2 == 0)
-        {
+    public static boolean isEvenNumber(int num) {
+        if (num % 2 == 0) {
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
-
     }
 }
