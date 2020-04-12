11ddb4398d31c593963f4b2183a40077b9b337fd
Radhika Santapuri <radhika.santapuri@gmail.com>
2020-04-11 20:31:11 -0700

    coding exercises

---
+++ PersonalProjects/src/ExercisesPartTwo/DiagonalStar.java
@@ -0,0 +1,93 @@
+package ExercisesPartTwo;
+
+public class DiagonalStar {
+    /**
+     * Write a method named printSquareStar with one parameter of type int named number.
+     * If number is < 5, the method should print "Invalid Value".
+     * The method should print diagonals to generate a rectangular pattern composed of stars (*). This should be accomplished by using loops (see examples below).
+     * EXAMPLE INPUT/OUTPUT:
+     * EXAMPLE 1
+     * printSquareStar(5); should print the following:
+     * → NOTE: For text in Code Blocks below, use code icon {...}  on Udemy
+     * *****
+     * ** **
+     * * * *
+     * ** **
+     * *****
+     * Explanation:
+     * *****   5 stars
+     * ** **   2 stars space 2 stars
+     * * * *   1 star space 1 star space 1 star
+     * ** **   2 stars space 2 stars
+     * *****   5 stars
+     * EXAMPLE 2
+     * printSquareStar(8); should print the following:
+     * ********
+     * **    **
+     * * *  * *
+     * *  **  *
+     * *  **  *
+     * * *  * *
+     * **    **
+     * ********
+     * The patterns above consist of a number of rows and columns (where number is the number of rows to print). For each row or column, stars are printed based on four conditions (Read them carefully):
+     * * In the first or last row
+     * * In the first or last column
+     * * When the row number equals the column number
+     * * When the column number equals rowCount - currentRow + 1 (where currentRow is current row number)
+     * HINT: Use a nested loop (a loop inside of a loop).
+     *
+     * HINT: To print on the same line, use the print method instead of println, e.g. System.out.print(" "); prints a space and does not "move" to another line.
+     *
+     * HINT: To "move" to another line, you can use an empty println call, e.g. System.out.println(); .
+     *
+     * NOTE: The method printSquareStar should be defined as public static like we have been doing so far in the course.
+     *
+     * NOTE: Do not add a main method to the solution code.
+     */
+
+    public static void main(String[] args) {
+        printSquareStar(5);
+    }
+
+    public static void printSquareStar(int number) {
+        if(number<5) {
+            System.out.println("Invalid Value");
+        }
+        if(number % 2 != 0) {
+            int i = 1;
+            int j = 1;
+            while (i <= number){
+                System.out.print("*");
+                i++;
+            }
+            System.out.println(" ");
+            while (j < number) {
+                System.out.print("*");
+                j++;
+                if(j == (number - 2)){
+                    System.out.print(" ");
+                }
+
+            }
+
+
+//            for (int i = 1; i <= number; i++) {
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//            int space = number-4;
+//            //line 2
+//            for(int i = 1; i<=2; i++){
+//                System.out.print("*");
+//            }
+//            for(int i=1; i<=space; i++){
+//                System.out.print(" ");
+//            }
+//            for(int i=1; i<=2; i++){
+//                System.out.print("*");
+//            }
+
+        }
+    }
+}
--- PersonalProjects/src/ExercisesPartTwo/LargestPrimeNumber.java
+++ PersonalProjects/src/ExercisesPartTwo/LargestPrimeNumber.java
@@ -1,4 +1,120 @@
 package ExercisesPartTwo;
 
 public class LargestPrimeNumber {
+    /**
+     * Write a method named getLargestPrime with one parameter of type int named number.
+     *
+     * If the number is negative or does not have any prime numbers, the method should return -1 to indicate an invalid value.
+     *
+     * The method should calculate the largest prime factor of a given number and return it.
+     *
+     *
+     * EXAMPLE INPUT/OUTPUT:
+     *
+     * * getLargestPrime (21); should return 7 since 7 is the largest prime (3 * 7 = 21)
+     *
+     * * getLargestPrime (217); should return 31 since 31 is the largest prime (7 * 31 = 217)
+     *
+     * * getLargestPrime (0); should return -1 since 0 does not have any prime numbers
+     *
+     * * getLargestPrime (45); should return 5 since 5 is the largest prime (3 * 3 * 5 = 45)
+     *
+     * * getLargestPrime (-1); should return -1 since the parameter is negative
+     *
+     *
+     * HINT: Since the numbers 0 and 1 are not considered prime numbers, they cannot contain prime numbers.
+     *
+     * NOTE: The method getLargestPrime should be defined as public static like we have been doing so far in the course.
+     *
+     * NOTE: Do not add a main method to the solution code.
+     */
+
+    public static void main(String[] args) {
+        int largestPrime = getLargestPrime (16);
+        System.out.println(largestPrime);
+    }
+    public static int getLargestPrime(int number) {
+        int num = 0;
+        int n = 0;
+        int i = 1;
+        if(number<=1){
+            return -1;
+        }
+        else if(number == 2) {
+            return number;
+        }
+        else if(number <= 10) {
+            i++;
+            while (number > 2) {
+                if((number % i) == 1) {
+                    if(number/i == 0) {
+                        System.out.println("odd number divisible by a number");
+                    }
+                    else {
+                        return number;
+                    }
+                }
+                else {
+                    num = number/i;
+                    return num;
+                }
+                if(i > number) {
+                    break;
+                }
+            }
+        }
+
+        while (number > 1) {
+            int j = 1;
+            if((number % i) == 0) {
+                if((i % 2) != 0) {
+                    while (i > j) {
+                        j++;
+                        if((i % j) == 0){
+                            n = i/j;
+                            num = n;
+                            break;
+                        }
+                        else {
+                            num = i;
+                        }
+                        if(j >= i/2) {
+                            break;
+                        }
+                    }
+                }
+                if(i % 2 == 0) {
+                    int k = 1;
+                    while (i > 0) {
+                        k++;
+                        if(i % k == 0) {
+                            if(k%2==0){
+                                num = 2;
+                            }
+                            else{
+                                num = i/k;
+                                if(k > num) {
+                                    num = k;
+                                    break;
+                                }
+
+                            }
+
+                        }
+                        if(k > i/2){
+                            break;
+                        }
+                    }
+                }
+            }
+            i++;
+            if(num == 0) {
+                num = number;
+            }
+            if(i > number/2){
+                break;
+            }
+        }
+        return num;
+    }
 }
